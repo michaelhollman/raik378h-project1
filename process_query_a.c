713126dd1d8e08c0f56f9fdd1cbfd9ca71349a9b
Darren Johnson <darrenrj56@gmail.com>
2014-02-27 00:21:25 -0600

    First two queries, ready for your input!
    Here's some queries.

---
+++ process_query_a.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/times.h>
+#include <string.h>
+
+#include "record.h"
+
+
+int main(int argc, char **argv)
+{
+    int i;
+	int nebraskaCount;
+	nebraskaCount = 0;
+	int locationID:
+	int mid;
+	int first;
+	int last;
+	mid = 0;
+	first = 0;
+	last = 0;
+	locationID = 0;
+	char nebraska[]  = "Nebraska";
+    /* print usage if needed */
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s total_user_number\n", argv[0]);
+        exit(0);
+    }
+    
+    /* get total record number */
+    int total_user_number = atoi(argv[1]);
+	int total_location_number = atoi(argv[2]);
+    
+
+    char filename[1024];
+	char locfilename[1024];
+    FILE *fp = NULL;
+	FILE *lf = NULL;
+    
+    struct timeval time_start, time_end;
+    
+    /* start time */
+    gettimeofday(&time_start, NULL);
+    
+    for (i = 0; i < total_user_number; i++) {
+        /* open the corresponding file */  
+        sprintf(filename, "user_%06d.dat", i);
+    
+        fp = fopen(filename,"rb");
+    
+        if (!fp) {
+            fprintf(stderr, "Cannot open %s\n", filename);
+            exit(0);
+        }
+        
+        /* read the record from the file */
+        //TODO Insert Casseys method to read the file
+		user_t *rp = read_record(fp);
+			
+        /* =========== start of data processing code ================ */
+		locationID = rp-> locationId;
+		last = total_location_number;
+		
+		while (first <= last){
+			int mid = (first + last) /2;
+			spintf(locfilename, "location_%06d.dat",mid);
+		
+			lf = fopen(locfilename,"rb");
+			//location_t *lp = readlocationmethod(lf);
+			if (locationID > lp->locationId){
+				first = mid + 1;
+				fclose(lf);
+			}
+			else if (locationID < lp->locationId){
+				last = mid - 1;
+				fclose(lf);
+			}
+			else {
+				if (strcmp(lp->state, nebraska) == 0){
+					nebraskaCount += 1;
+				}
+				fclose(lf);
+				}
+		}
+	}
+		
+        
+        
+        /* =========== end of data processing code ================ */    
+    
+        /* free memory */
+        free_record(rp);
+    
+        /* close the file */
+        fclose(fp);
+    }    
+        
+    printf("count is %d", nebraskaCount);
+    /* end time */
+    gettimeofday(&time_end, NULL);
+    
+    float totaltime = (time_end.tv_sec - time_start.tv_sec)
+                    + (time_end.tv_usec - time_start.tv_usec) / 1000000.0f;
+                    
+         
+                    
+    printf("\n\nProcess time %f seconds\n", totaltime);
+    
+    return 0;
+}
---
+++ process_query_b.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/times.h>
+
+#include "record.h"
+
+
+int main(int argc, char **argv)
+{
+    int i;
+int j;
+j = 0;
+int timeCount = 0;
+int messageCount = 0;
+int messageFound = 0;
+int timestampID = 0;
+int userID = 0;
+int last = 0;
+int first = 0;
+int mid = 0;
+
+
+    /* print usage if needed */
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s total_record_number\n", argv[0]);
+        exit(0);
+    }
+    
+    /* get total record number */
+    int total_message_number = atoi(argv[1]);
+	int total_timestamp_number = atoi(argv[2]);
+    
+
+    char filename[1024];
+	char tsfilename[1024];
+    FILE *fp = NULL;
+	FILE *ts = NULL:
+	
+    
+    struct timeval time_start, time_end;
+    
+    /* start time */
+    gettimeofday(&time_start, NULL);
+    
+    for (i = 0; i < total_message_number; i++) {
+        /* open the corresponding file */  
+	        sprintf(filename, "record_%06d.dat", i);
+    
+        fp = fopen(filename,"rb");
+    
+        if (!fp) {
+            fprintf(stderr, "Cannot open %s\n", filename);
+            exit(0);
+        }
+        
+        /* read the record from the file */
+		//Cassey's read method
+        record_t *rp = read_record(fp);
+		timestampID = rp->timestampId;
+		userID = rp->userId;
+	    		
+        /* =========== start of data processing code ================ */
+		last = total_timestamp_number;
+		
+		while (first <= last){
+			int mid = (first + last) /2;
+			spintf(tsfilename, "timestamp_%06d.dat",mid);
+		
+			ts = fopen(tsfilename,"rb");
+			//location_t *lp = readtimestampmethod(ts);
+			if (timestampID > ts->timestampId){
+				first = mid + 1;
+				fclose(ts);
+			}
+			else if (timestampID > ts->timestampId){
+				last = mid - 1;
+				fclose(ts);
+			}
+			else {
+				if (ts->hour == 8){
+					//TODO insert trippy michael hash function
+					timeCount +=1;
+				}
+				fclose(ts);
+				}
+		}
+		
+
+	    //print_record(rp);
+        
+        /* =========== end of data processing code ================ */    
+    
+        /* free memory */
+        free_record(rp);
+    
+        /* close the file */
+        fclose(fp);
+    }    
+        
+    printf("count is %d", timeCount);
+    /* end time */
+    gettimeofday(&time_end, NULL);
+    
+    float totaltime = (time_end.tv_sec - time_start.tv_sec)
+                    + (time_end.tv_usec - time_start.tv_usec) / 1000000.0f;
+                    
+         
+                    
+    printf("\n\nProcess time %f seconds\n", totaltime);
+    
+    return 0;
+}
