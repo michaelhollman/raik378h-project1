089f1b3b07c1cd4634c46b14a8b77980ab8fbc08
Darren Johnson <darrenrj56@gmail.com>
2014-02-27 21:17:35 -0600

    Query_B new approach

--- query_b.c
+++ query_b.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <stdbool.h>
 
-#include "record.h"
 #include "user.h"
 #include "message.h"
 #include "location.h"
@@ -12,8 +12,6 @@ int main(int argc, char **argv)
 {
 int i;
 int timeCount = 0;
-int messageCount = 0;
-int messageFound = 0;
 int timestampID = 0;
 int userID = 0;
 int last = 0;
@@ -23,19 +21,73 @@ int mid = 0;
 
     /* print usage if needed */
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s total_message_number total_timestamp_number\n", argv[0]);
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s total_message_number total_timestamp_number total_user_number\n", argv[0]);
         exit(0);
     }
 
     /* get total record number */
     int total_message_number = atoi(argv[1]);
 	int total_timestamp_number = atoi(argv[2]);
+	int total_user_number = atoi(argv[3]);
 
     struct timeval time_start, time_end;
 
     /* start time */
     gettimeofday(&time_start, NULL);
+
+	bool validTime[total_timestamp_number];
+	bool validUser[total_user_number];
+
+	for (i = 0; i < total_user_number; i ++){
+		validUser[i] = false;
+	}
+
+	first = 0;
+	last = total_timestamp_number;
+	timestamp_t *tsp;
+
+	while (first <= last){
+		mid = (first + last) /2;
+		tsp = read_timestamp(mid);
+
+		if (tsp->hour < 8){
+			first = mid + 1;
+		}
+		else if (tsp->hour > 9 || tsp->hour ==9 && tsp-> minute > 0 ){
+			last = mid - 1;
+		}
+		else {
+			if (tsp->hour == 8 || (tsp-> hour ==9 && tsp->minute == 0)){
+					timeCount +=1;
+			}
+		}
+		free_timestamp(tsp);
+	}
+
+
+	for (i = mid; i < total_timestamp_number; i++){
+		tsp = read_timestamp(i);
+		if (tsp->hour == 8 || (tsp-> hour ==9 && tsp->minute == 0)){
+			 validTime[i] = true;
+		}
+		else {
+			validTime[i] = false;
+		}
+		free_timestamp(tsp);
+
+	}
+
+	for (i = mid-1; i >= 0; i--){
+		tsp = read_timestamp(i);
+		if (tsp->hour == 8 || (tsp-> hour ==9 && tsp->minute == 0)){
+			 validTime[i] = true;
+		}
+		else {
+			validTime[i] = false;
+		}
+		free_timestamp(tsp);
+	}
 
     for (i = 0; i < total_message_number; i++) {
         /* read the record from the file */
@@ -43,36 +95,16 @@ int mid = 0;
 		timestampID = msgp->timestampId;
 		userID = msgp->userId;
 
-		last = total_timestamp_number;
-
-		while (first <= last){
-			int mid = (first + last) /2;
-			timestamp_t *tsp = read_timestamp(mid);
-
-			if (timestampID > tsp->timestampId){
-				first = mid + 1;
-				fclose(tsfp);
+		if (validTime[timestampID]){
+			if (validUser[userID]){
+				timeCount ++;
 			}
-			else if (timestampID > tsp->timestampId){
-				last = mid - 1;
-				fclose(tsfp);
+			else{
+              validUser[userID] = true;
 			}
-			else {
-				if (tsp->hour == 8 || (tsp-> hour ==9 && tsp->minute == 0)){
-					//TODO insert trippy michael hash function
-					timeCount +=1;
-				}
-				fclose(tsfp);
-				}
-
-        free_timestamp(tsp);
-
 		}
 
 		free_message(msgp);
-
-        /* close the file */
-        fclose(msgfp);
     }
 
     printf("count is %d", timeCount);
