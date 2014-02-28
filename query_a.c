c1fcf1bfb59d8cc8aaa5c42638ff214a1aa6fbf2
sjohnson8 <s.johnson1120@yahoo.com>
2014-02-27 22:01:01 -0600

    queries a c and d

--- query_a.c
+++ query_a.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "record.h"
 #include "user.h"
@@ -11,7 +12,6 @@
 
 int main(int argc, char **argv)
 {
-    int i;
     int nebraskaCount;
     nebraskaCount = 0;
     int locationID;
@@ -34,56 +34,65 @@ int main(int argc, char **argv)
     int total_user_number = atoi(argv[1]);
     int total_location_number = atoi(argv[2]);
 
-
-    char filename[1024];
-    char locfilename[1024];
-    FILE *fp = NULL;
-    FILE *lf = NULL;
-
     struct timeval time_start, time_end;
 
     /* start time */
     gettimeofday(&time_start, NULL);
-
-    for (i = 0; i < total_user_number; i++) {
-
-        /* read the record from the file */
-        //TODO Insert Casseys method to read the file
-        user_t *up = read_user(i);
-
-        /* =========== start of data processing code ================ */
-        locationID = up-> locationId;
-        last = total_location_number;
-
-        while (first <= last){
-            int mid = (first + last) /2;
-
-            if (!lf) {
-                fprintf(stderr, "Cannot open %s\n", locfilename);
-                exit(0);
-            }
-
-            location_t *lp = read_location(mid);
-            if (locationID > lp->locationId){
-                first = mid + 1;
-                fclose(lf);
-            }
-            else if (locationID < lp->locationId){
-                last = mid - 1;
-                fclose(lf);
-            }
-            else {
-                if (strcmp(lp->state, nebraska) == 0){
-                    nebraskaCount += 1;
-                }
-                fclose(lf);
-                }
-                free_location(lp);
-            }
-
-               free_user(up);
-               fclose(fp);
-        }
+	
+	/********* my code ********/
+	bool validId[total_location_number];
+	int j;
+	for(j = 0; j < total_location_number; j++){
+		validId[j] = false;
+	}
+	
+	last = total_location_number - 1;
+	while(first <= last){
+		mid = (first + last) / 2;
+		location_t *locPnt = read_location(mid);
+		if(strcmp(locPnt->state, nebraska) == 0){
+			validId[mid] = true;
+			free_location(locPnt);
+			for(j = mid - 1; j >= first; j--){
+				locPnt = read_location(j);
+				if(strcmp(locPnt->state, nebraska) == 0){
+					validId[j] = true;
+				}
+				else{
+					j = -1; //breaks the loop
+				}
+				free_location(locPnt);
+			}
+			for(j = mid + 1; j <= last; j++){
+				locPnt = read_location(j);
+				if(strcmp(locPnt->state, nebraska) == 0){
+					validId[j] = true;
+				}
+				else{
+					j = last + 1; //breaks the loop
+				}
+				free_location(locPnt);
+			}
+		}
+		else if(strcmp(locPnt->state, nebraska) < 0){
+			first = mid + 1;
+			free_location(locPnt);
+		}
+		else{
+			last = mid - 1;
+			free_location(locPnt);
+		}
+	}
+	
+	for(j = 0; j < total_user_number; j++){
+		user_t *user = read_user(j);
+		if(validId[user->locationId]){
+			nebraskaCount++;
+		}
+		free_location(user);
+	}
+	
+	/********* end my code ********/
 
     printf("count is %d", nebraskaCount);
     /* end time */
--- query_c.c
+++ query_c.c
@@ -1,128 +1,187 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <string.h>
+#include <stdbool.h>
 
-#include "location.h"
-#include "message.h"
-#include "timestamp.h"
+#include "record.h"
 #include "user.h"
+#include "message.h"
+#include "location.h"
+#include "timestamp.h"
 
 int main(int argc, char **argv)
 {
-int i =0;
-int count = 0;
-int messageFound = 0;
-int timestampID = 0;
-int locationID = 0;
-int userID = 0;
-int last = 0;
-int first = 0;
-int mid = 0;
-char nebraska[]  = "Nebraska";
-
+    int count;
+    count = 0;
+    int locationID;
+    int mid;
+    int first;
+    int last;
+    mid = 0;
+    first = 0;
+    last = 0;
+    locationID = 0;
+    char nebraska[]  = "Nebraska";
 
     /* print usage if needed */
-   if (argc != 5) {
-        msgrintf(stderr, "Usage: %s total_message_number total_timestamp_number total_user_number total_location_number\n", argv[0]);
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s total_user_number total_location_number total_message_number total_timestamp_number\n", argv[0]);
         exit(0);
     }
-    
+
     /* get total record number */
-    int total_message_number = atoi(argv[1]);
-	int total_timestamp_number = atoi(argv[2]);
-	int total_user_number = atoi(argv[3]);
-	int total_location_number = atoi(argv[4]);
+    int total_user_number = atoi(argv[1]);
+    int total_location_number = atoi(argv[2]);
+	int total_message_number = atoi(argv[3]);
+	int total_timestamp_number = atoi(argv[4]);
 
     struct timeval time_start, time_end;
-    
+
     /* start time */
     gettimeofday(&time_start, NULL);
-    
-    for (i = 0; i < total_message_number; i++) {
-        
-        /* read the record from the file */
-        message_t *messagePtr = read_message(i); 
-		timestampID = messagePtr->timestampID;
-		userID = messagePtr->userId;
-
-		first = 0;
-		last = total_timestamp_number;
-		//check if hour is equal to 8
-		while (first <= last){
-			int mid = (first + last) /2;
-
-			timestamp_t *timestampPtr = read_timestamp(mid);
-			
-			if (timestampID > timestampPtr->timestampID){
-				first = mid + 1;
-			}
-			else if (timestampID > timestampPtr->timestampID){
-				last = mid - 1;
-			}
-			else {
-				if (timestampPtr->hour == 8 || (timestampPtr-> hour ==9 && timestampPtr->minute == 0)){
-					last = total_user_number;
-					first = 0;
-					
-					//find the userid so we can find the locationID
-					while (first <= last){
-						int mid = (first + last) /2;
-												
-						user_t *userPtr = read_user(mid);
-
-						if (userID > userPtr->userId){
-							first = mid + 1;
-						}
-						else if (userID > userPtr->userId){
-							last = mid - 1;
-						}
-						else {
-							if (userID == userPtr->userId){
-								locationID = userPtr->locationId;
-								
-								first = 0;
-								last = total_location_number;
-								
-								while (first <= last){
-									int mid = (first + last) /2;
-																		
-									location_t *locationPtr = read_location(mid);
-									if (locationID > locationPtr->locationId){
-										first = mid + 1;
-									}
-									else if (locationID < locationPtr->locationId){
-										last = mid - 1;
-									}
-									else {
-										if (strcmp(locationPtr->state, nebraska) == 0){
-											//TODO insert trippy michael function to increment a counter if the user hasn't already been counted
-											count += 1;
-										}
-									}			
-								free_location(locationPtr);
-								}
-							}
-						}
-						free_user(userPtr);
-					}
+	
+	/********* my code ********/
+	bool validLocationId[total_location_number];
+	int j;
+	for(j = 0; j < total_location_number; j++){
+		validLocationId[j] = false;
+	}
+	
+	last = total_location_number - 1;
+	while(first <= last){
+		mid = (first + last) / 2;
+		location_t *locPnt = read_location(mid);
+		if(strcmp(locPnt->state, nebraska) == 0){
+			validLocationId[mid] = true;
+			free_location(locPnt);
+			for(j = mid - 1; j >= first; j--){
+				locPnt = read_location(j);
+				if(strcmp(locPnt->state, nebraska) == 0){
+					validLocationId[j] = true;
 				}
+				else{
+					j = -1; //breaks the loop
+				}
+				free_location(locPnt);
 			}
-			free_timestamp(timestampPtr);
+			for(j = mid + 1; j <= last; j++){
+				locPnt = read_location(j);
+				if(strcmp(locPnt->state, nebraska) == 0){
+					validLocationId[j] = true;
+				}
+				else{
+					j = last + 1; //breaks the loop
+				}
+				free_location(locPnt);
+			}
 		}
-		free_message(messagePtr);
+		else if(strcmp(locPnt->state, nebraska) < 0){
+			first = mid + 1;
+			free_location(locPnt);
+		}
+		else{
+			last = mid - 1;
+			free_location(locPnt);
+		}
+	}
+	
+	bool validUserID[total_user_number];
+	for(j = 0; j < total_user_number; j++){
+		user_t *user = read_user(j);
+		if(validLocationId[user->locationId]){
+			validUserID[user->userId] = true;
+		}
+		else{
+			validUserID[user->userId] = false;
+		}
+		free_location(user);
+	}
+	
+	bool validTime[total_timestamp_number];
+	for (i = 0; i < total_timestamp_number; i ++){
+		validTime[i] = false;
+	}
 
-    }    
-        
+	first = 0;
+	last = total_timestamp_number;
+	timestamp_t *tsp;
+
+	while (first <= last){
+		mid = (first + last) /2;
+		tsp = read_timestamp(mid);
+
+		if (tsp->hour < 8){
+			first = mid + 1;
+		}
+		else if (tsp->hour > 9 || tsp->hour ==9 && tsp-> minute > 0 ){
+			last = mid - 1;
+		}
+		else {
+			first = last + 1;
+		}
+		free_timestamp(tsp);
+	}
+
+
+	for (i = mid; i < total_timestamp_number; i++){
+		tsp = read_timestamp(i);
+		if (tsp->hour == 8 || (tsp-> hour ==9 && tsp->minute == 0)){
+			 validTime[i] = true;
+		}
+		else{
+            i = total_timestamp_number;
+		}
+		free_timestamp(tsp);
+
+	}
+
+	for (i = mid-1; i >= 0; i--){
+		tsp = read_timestamp(i);
+		if (tsp->hour == 8 || (tsp-> hour ==9 && tsp->minute == 0)){
+			 validTime[i] = true;
+		}
+		else{
+            i = -1;
+		}
+		free_timestamp(tsp);
+	}
+	
+	
+	for(j = 0; j < total_message_number; j++){
+		message_t *message = read_message(j);
+		if(validTime[message->timestampId]){
+			if(validUserID[message->userId] > -1){
+				validUserID[message->userId]++;
+			}
+		}
+		free_location(message);
+	}
+	
+	
+	for(j = 0; j < total_message_number; j++){
+		message_t *message = read_message(j);
+		if(validTime[message->timestampId]){
+			if(validUserID[message->userId]){
+				count++;
+				validUserID[message->userId] = false;
+			}
+		}
+		free_location(message);
+	}
+	
+	/********* end my code ********/
+
     printf("count is %d", count);
     /* end time */
     gettimeofday(&time_end, NULL);
-    
+
     float totaltime = (time_end.tv_sec - time_start.tv_sec)
                     + (time_end.tv_usec - time_start.tv_usec) / 1000000.0f;
-                    
-         
-                    
+
+
+
     printf("\n\nProcess time %f seconds\n", totaltime);
-    
+
     return 0;
 }
