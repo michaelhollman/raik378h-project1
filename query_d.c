077bd821a70b03417398827f334b02c74dd8c958
Michael Hollman <hollyfrass@gmail.com>
2014-03-02 12:55:04 -0600

    I really should make my commit messages meaningfull... or not...

--- query_d.c
+++ query_d.c
@@ -4,183 +4,146 @@
 #include <string.h>
 #include <stdbool.h>
 
-#include "record.h"
 #include "user.h"
 #include "message.h"
-#include "location.h"
+#include "state.h"
 #include "timestamp.h"
+#include "file_count.h"
 
 int main(int argc, char **argv)
 {
-    int count;
-    count = 0;
-    int locationID;
-    int mid;
-    int first;
-    int last;
-    int i;
-    mid = 0;
-    first = 0;
-    last = 0;
-    locationID = 0;
-    char nebraska[]  = "Nebraska";
+    // time the program
+    clock_t startTime = clock();
+    struct timeval sysTimeStart, sysTimeEnd;
+    gettimeofday(&sysTimeStart, NULL);
+    
+    int nebraskaStateId, first, last, mid, i, maxUserId, max = 0;
+    char nebraskaStr[]  = "Nebraska";
+    
+    // get file counts
+    file_count_t *fc = read_file_count();
+    int userCount = fc->users;
+    int stateCount = fc->states;
+	int messageCount = fc->messages;
+	int timestampCount = fc->timestamps;
+    free_file_count(fc);
 
-    /* print usage if needed */
-    if (argc != 5) {
-        fprintf(stderr, "Usage: %s total_user_number total_location_number total_message_number total_timestamp_number\n", argv[0]);
-        exit(0);
-    }
-
-    /* get total record number */
-    int total_user_number = atoi(argv[1]);
-    int total_location_number = atoi(argv[2]);
-	int total_message_number = atoi(argv[3]);
-	int total_timestamp_number = atoi(argv[4]);
-
-    struct timeval time_start, time_end;
-
-    /* start time */
-    gettimeofday(&time_start, NULL);
-
-	/********* my code ********/
-	bool validLocationId[total_location_number];
-	int j;
-	for(j = 0; j < total_location_number; j++){
-		validLocationId[j] = false;
+    // arrays to keep track of valid times, users, etc.
+	bool validTimes[timestampCount];
+    bool validUsers[userCount];
+    int userMessageCount[userCount];
+    for (i = 0; i < timestampCount; i ++)
+    {
+		validTimes[i] = false;
 	}
-
-	last = total_location_number - 1;
-	while(first <= last){
-		mid = (first + last) / 2;
-		location_t *locPnt = read_location(mid);
-		if(strcmp(locPnt->state, nebraska) == 0){
-			validLocationId[mid] = true;
-			free_location(locPnt);
-			for(j = mid - 1; j >= first; j--){
-				locPnt = read_location(j);
-				if(strcmp(locPnt->state, nebraska) == 0){
-					validLocationId[j] = true;
-				}
-				else{
-					j = -1; //breaks the loop
-				}
-				free_location(locPnt);
-			}
-			for(j = mid + 1; j <= last; j++){
-				locPnt = read_location(j);
-				if(strcmp(locPnt->state, nebraska) == 0){
-					validLocationId[j] = true;
-				}
-				else{
-					j = last + 1; //breaks the loop
-				}
-				free_location(locPnt);
-			}
-			last = first -1;
-		}
-		else if(strcmp(locPnt->state, nebraska) < 0){
-			first = mid + 1;
-			free_location(locPnt);
-		}
-		else{
-			last = mid - 1;
-			free_location(locPnt);
-		}
-	}
-
-	int validUserID[total_user_number];
-	for(j = 0; j < total_user_number; j++){
-		user_t *user = read_user(j);
-		if(validLocationId[user->locationId]){
-			validUserID[user->userId] = 0;
-		}
-		else{
-			validUserID[user->userId] = -1;
-		}
-		free_user(user);
-	}
-
-	bool validTime[total_timestamp_number];
-	for (i = 0; i < total_timestamp_number; i ++){
-		validTime[i] = false;
-	}
-
+    // user arrays initialized later
+    
+    // binary search states to get Nebraska's ID
 	first = 0;
-	last = total_timestamp_number;
-	timestamp_t *tsp;
-
-	while (first <= last){
-		mid = (first + last) /2;
-		tsp = read_timestamp(mid);
-
-		if (tsp->hour < 8){
+    last = stateCount - 1;
+    nebraskaStateId = -1;
+	while (first <= last && nebraskaStateId == -1)
+    {
+		mid = (first + last) / 2;
+		state_t *state = read_state(mid);
+        
+		if (strcmp(state->name, nebraskaStr) == 0)
+        {
+            nebraskaStateId = state->stateId;
+		}
+		else if (strcmp(state->name, nebraskaStr) < 0)
+        {
 			first = mid + 1;
 		}
-		else if (tsp->hour > 9 || tsp->hour ==9 && tsp-> minute > 0 ){
+		else
+        {
 			last = mid - 1;
 		}
-		else {
-			first = last + 1;
-		}
-		free_timestamp(tsp);
+        
+        free_state(state);
 	}
 
-
-	for (i = mid; i < total_timestamp_number; i++){
-		tsp = read_timestamp(i);
-		if (tsp->hour == 8 || (tsp-> hour ==9 && tsp->minute == 0)){
-			 validTime[i] = true;
-		}
-		else{
-            i = total_timestamp_number;
-		}
-		free_timestamp(tsp);
-
+    // mark all users from Nebraska as valid, else invalid
+    for (i = 0; i < userCount; i++)
+    {
+		user_t *user = read_user(i);
+        validUsers[user->userId] = (user->stateId == nebraskaStateId);
+        userMessageCount[user->userId] = 0;
+        free_user(user);
 	}
 
-	for (i = mid-1; i >= 0; i--){
-		tsp = read_timestamp(i);
-		if (tsp->hour == 8 || (tsp-> hour ==9 && tsp->minute == 0)){
-			 validTime[i] = true;
+	// binary search for a valid timestamp
+	first = 0;
+	last = timestampCount;
+    bool found = false;
+	while (first <= last && !found)
+    {
+		mid = (first + last) / 2;
+        timestamp_t *tsp = read_timestamp(mid);
+        
+		if (tsp->hour < 8)
+        {
+			first = mid + 1;
 		}
-		else{
-            i = -1;
+		else if (tsp->hour > 9 || (tsp->hour == 9 && tsp->minute > 0))
+        {
+			last = mid - 1;
 		}
+		else // found
+        {
+            found = true;
+        }
 		free_timestamp(tsp);
 	}
+    
+    // mark valid times above and below as valid
+    int direction = 1;
+    bool done = false;
+	for (i = mid; i < timestampCount && !done; i += direction)
+    {
+		timestamp_t *tsp = read_timestamp(i);
+		if (tsp->hour == 8 || (tsp->hour == 9 && tsp->minute == 0))
+        {
+            validTimes[tsp->timestampId] = true;
+		}
+		else if (direction == 1) //reset, switch directions
+        {
+            i = mid;
+            direction = -1;
+        }
+        else
+        {
+            done = true;
+		}
+        free_timestamp(tsp);
+	}
 
-
-	for(j = 0; j < total_message_number; j++){
-		message_t *message = read_message(j);
-		if(validTime[message->timestampId]){
-			if(validUserID[message->userId] > -1){
-				validUserID[message->userId]++;
-			}
+	// loop through all messages, count valid messages, keep track of max and max's id
+	for(i = 0; i < messageCount; i++)
+    {
+		message_t *message = read_message(i);
+		if(validTimes[message->timestampId] && validUsers[message->userId])
+        {
+            userMessageCount[message->userId]++;
+            if (userMessageCount[message->userId] > max)
+            {
+                max = userMessageCount[message->userId];
+                maxUserId = message->userId;
+            }
 		}
 		free_message(message);
 	}
 
-	int max = 0;
-	int maxId = -1;
-	for(j = 0; j < total_user_number; j++){
-		if(validUserID[j] > max){
-			max = validUserID[j];
-			maxId = j;
-		}
-	}
-
-	/********* end my code ********/
-
-    printf("id of the max nebraskan 8 to 9 sender is %d", maxId);
-    /* end time */
-    gettimeofday(&time_end, NULL);
-
-    float totaltime = (time_end.tv_sec - time_start.tv_sec)
-                    + (time_end.tv_usec - time_start.tv_usec) / 1000000.0f;
-
-
-
-    printf("\n\nProcess time %f seconds\n", totaltime);
-
+    printf("User from Nebraska who sent most messages from 8-9: %d  (%d messages)\n", maxUserId, max);
+    
+    // end timing the program
+    clock_t endTime = clock();
+    double totaltime = (double)(endTime - startTime)/CLOCKS_PER_SEC;
+    printf("\nProcess time %f seconds (time.h)\n", totaltime);
+    gettimeofday(&sysTimeEnd, NULL);
+    float totaltime2 = (sysTimeEnd.tv_sec - sysTimeStart.tv_sec)
+    + (sysTimeEnd.tv_usec - sysTimeStart.tv_usec) / 1000000.0f;
+    printf("Process time %f seconds (sys/time.h)\n", totaltime2);
+    
     return 0;
 }
